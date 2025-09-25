f5f8203761fad39e9b84b923ec46a75855f67860
Pulkit Rathi <pulkitr2002@gmail.com>
2025-09-25 17:27:55 +0530

    learn arrow function, this keyword & scopes

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -8,10 +8,62 @@ if(true){
     let a = 10
     const b = 11
     // var c = 20 // this creates problem
-    console.log("Inner:", a);
-    
+    // console.log("Inner:", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// Nested Scopes in JS
+
+// Closures -> In nested functions, child function can access variables of parent function
+
+function one(){
+    const username = "billu"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+
+// Nested if-else statments
+if(true){
+    const username = "billu"
+
+    if(username === "billu"){
+        const website = " chatgpt"
+        // console.log(username + website);
+    }
+    // console.log(website); // error here
+}
+
+// console.log(username); // error here
+
+
+
+// ----------------------- interesting example ------------------
+
+// This is normal function
+
+addOne(3) // this can be accessed here 
+function addOne(num){
+    return num + 1
+}
+
+
+// This is expression and also a function
+// Variables in JS can hold anything inside it
+
+addTwo(4) // this is cannot be accessed here
+const addTwo = function(num){
+    return num + 2
+}
---
+++ 03_basics/04_arrow.js
@@ -0,0 +1,70 @@
+const user = {
+    username: "billu",
+    price: 877,
+
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        // this.username -> refers to current context
+        console.log(this); // refers to current context or values
+        
+    }
+}
+
+// here, context means value
+
+// user.welcomeMessage
+// user.welcomeMessage()
+// user.username = "john"
+// user.welcomeMessage()
+
+// console.log(this); // output -> {} (empty global object)
+
+
+// Function for this
+
+// function chai(){
+//     let username = "billu"
+     // console.log(this.username); // output -> undefined
+     // we cannot use 'this' inside the function
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "billu"
+//     console.log(this.username); // output -> undefined
+// }
+
+
+
+// Arrow function -> ES6 feature
+// Another way to define function in JS
+const chai = () => {
+    let username = "billu"
+    console.log(this.username); // output -> undefined
+    console.log(this); // output -> {} (empty object)
+}
+chai()
+
+
+// Simple Arrow function 
+// const addTwoNum = (num1, num2) => {
+//     return num1+num2
+// }
+
+// Implicit return
+// const addTwoNum = (num1, num2) => num1+num2
+
+// Another way of Implicit return
+// const addTwoNum = (num1, num2) => ( num1+num2 ) // Mostly used in Reactjs
+
+// Returning object in Implicit return
+const addTwoNum = (num1, num2) => ({username: "Pulkit"}) 
+
+console.log(addTwoNum(2,7));
+
+
+// Another example
+const myArray = [1,2,5,7,8]
+// myArray.forEach( function(){} ) //Normal function
+// myArray.forEach( () => {} ) // Arrow function
+// myArray.forEach( () => () ) // Implicit function
